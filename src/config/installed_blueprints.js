2d59c548d1783894485b759bc8230e07ba33eb12
Crowdbotics <team@crowdbotics.com>
2020-12-02 16:12:27 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList2181102', human_name: 'Notification List 2', access_route: 'NotificationList2181102'},
 { name: 'Maps3181101', human_name: 'Maps 3', access_route: 'Maps3181101', icon: 'map'},
 { name: 'SignIn21167503', human_name: 'Sign In 2 1', access_route: 'SignIn21167503'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList2181102Navigator from '../features/NotificationList2181102/navigator';
 import Maps3181101Navigator from '../features/Maps3181101/navigator';
 import SignIn21167503Navigator from '../features/SignIn21167503/navigator';
 
@@ -15,6 +16,7 @@ import SignIn21167503Navigator from '../features/SignIn21167503/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+NotificationList2181102: { screen: NotificationList2181102Navigator },
 Maps3181101: { screen: Maps3181101Navigator },
 SignIn21167503: { screen: SignIn21167503Navigator },
 
